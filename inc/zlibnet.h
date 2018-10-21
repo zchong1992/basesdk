326b0d3abefb401aadcd49ab1f208e3593ac56bb
zc <zc>
2018-10-21 13:48:48 +0800

    添加udp的接口实现

--- inc/zlibnet.h
+++ inc/zlibnet.h
@@ -3,66 +3,72 @@
 /******************************************
 *   author: zc							  *	
 *   date:  2018/2/26					  *
-*	´ËÎÄ¼þÎªÍøÂçÄ£¿éµÄ¼ò½à¿ª·¢¹¤¾ßÀà£¬    *
-*   ¿ìËÙ¿ª·¢Ê±£¬Ò»Ð©»ù´¡µÄÖØ¸´ÐÔ¹¦ÄÜÉèÖÃ  *
+*	ï¿½ï¿½ï¿½Ä¼ï¿½Îªï¿½ï¿½ï¿½ï¿½Ä£ï¿½ï¿½Ä¼ï¿½à¿ªï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½à£¬    *
+*   ï¿½ï¿½ï¿½Ù¿ï¿½ï¿½ï¿½Ê±ï¿½ï¿½Ò»Ð©ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ø¸ï¿½ï¿½Ô¹ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½  *
 *******************************************/
-
 
 #ifndef __ZLIBNET_H__
 #define __ZLIBNET_H__
 
 #include "global.h"
 namespace baseservice
-{
-	struct zlibNetAddr
-	{
-		unsigned int ip;
-		unsigned short port;
-        
-	};
-	class tcpClient{
-        public:
-        tcpClient();
-        ~tcpClient();
-        int init();
-        int release();
-        int connect(zlibNetAddr);
-        int connect(const char *ip,const char *port);
-        int connect(const char *ip,unsigned short port);
-        int send(const void * buf,int size,int flag);
-        int recv(void * buf,int bufSize,int flag);
-        int fd;
-	};
-	class tcpServer{
-        public:
-        tcpServer();
-        ~tcpServer();
-        int init();
-        int bind(zlibNetAddr);
-        int bind(const char *ip,const char *port);
-        int bind(const char *ip,unsigned short port);
-        int release();
-        int listen(int maxNum);
-        int accept(zlibNetAddr *);  
-        int accept();  
-        int fd;
-    };
-    class udpSocket
-    {
-    	udpSocket();
-        ~udpSocket();
-    	int init();
-        int recv();
-        int send();
-        int fd;
-    };
-        
+{
+
+struct zlibNetAddr
+{
+
+    unsigned int ip;
+    unsigned short port;
+};
+class tcpClient
+{
+
+  public:
+    tcpClient();
+    ~tcpClient();
+    int init();
+    int release();
+    int connect(zlibNetAddr);
+    int connect(const char *ip, const char *port);
+    int connect(const char *ip, unsigned short port);
+    int send(const void *buf, int size, int flag);
+    int recv(void *buf, int bufSize, int flag);
+    int fd;
+};
+class tcpServer
+{
+  public:
+    tcpServer();
+    ~tcpServer();
+    int init();
+    int bind(zlibNetAddr);
+    int bind(const char *ip, const char *port);
+    int bind(const char *ip, unsigned short port);
+    int release();
+    int listen(int maxNum);
+    int accept(zlibNetAddr *);
+    int accept();
+    int fd;
+};
+class udpSocket
+{
+  public:
+    udpSocket();
+    ~udpSocket();
+    int init();
+    int bind(zlibNetAddr);
+    int bind(const char *ip,const unsigned short port);
+    int bind(const char *ip,const char * port);
+
+    int recv(void *, int, zlibNetAddr*);
+    int recv(void *recvbuf, int buflen);
+
+    int send(void *, int, zlibNetAddr);
+    int send(void *sendbuf, int len, const char *ip, const unsigned short port);
+    int send(void *sendbuf, int len, const char *ip, const char *port);
+    int fd;
 };
 
-
-
+}; // namespace baseservice
 
 #endif
-
-
-
